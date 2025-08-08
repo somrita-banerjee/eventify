57d7e0a5aee5c725f4230ec32670be639ad6157d
somrita-banerjee <144698416+somrita-banerjee@users.noreply.github.com>
2025-08-08 19:46:47 +0530

    feat: enhance EventsController and EventsService with CRUD operations and error handling

--- backend/src/events/events.controller.ts
+++ backend/src/events/events.controller.ts
@@ -1,15 +1,39 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Patch, Delete } from '@nestjs/common';
 import { EventsService } from './events.service';
-import { ApiOperation } from '@nestjs/swagger';
 import { CreateEventDto } from './events.dto';
+import type { JwtUser } from 'src/decorator/userFromAuth.decorator';
+import { UserFromAuth } from 'src/decorator/userFromAuth.decorator';
 
 @Controller('events')
 export class EventsController {
-  constructor(private readonly eventsService: EventsService) {
+  constructor(private readonly eventsService: EventsService) {}
+  @Post()
+  create(
+    @Body() createEventDto: CreateEventDto,
+    @UserFromAuth() user: JwtUser,
+  ) {
+    return this.eventsService.createEvent(createEventDto);
   }
-  @ApiOperation({ summary: 'Create Event' })
-  @Post('create')
-  async createEvent(@Body() body: CreateEventDto) {
-    return await this.eventsService.createEvent(body);
+
+  @Get()
+  findAll() {
+    return this.eventsService.findAll();
+  }
+
+  @Get(':id') 
+  findOne(@Param('id') id: string) {
+    return this.eventsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string,
+    @Body() updateEventDto: CreateEventDto,
+    user: JwtUser) {
+    return this.eventsService.update(id, updateEventDto, user);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string, @UserFromAuth() user: JwtUser) {
+    return this.eventsService.delete(id, user);
   }
 }
--- backend/src/events/events.service.ts
+++ backend/src/events/events.service.ts
@@ -1,23 +1,142 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateEventDto } from './events.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { v4 as uuid } from 'uuid';
-
+import { JwtUser } from 'src/decorator/userFromAuth.decorator';
 
 @Injectable()
 export class EventsService {
-    constructor(private readonly prismaService: PrismaService ) {}
-    async createEvent(createEventDto: CreateEventDto) {
-        const event = await this.prismaService.events.create({
-            data: {
-                id: uuid(),
-                title: createEventDto.title,
-                description: createEventDto.description,
-                start_time: createEventDto.start_time,
-                end_time: createEventDto.end_time,
-                location: '',
-            },
-        });
-        return event;
-     }
+  logger = new Logger(EventsService.name);
+  constructor(private readonly prismaService: PrismaService) {}
+  async createEvent(createEventDto: CreateEventDto) {
+    try {
+      const event = await this.prismaService.events.create({
+        data: {
+          id: uuid(),
+          title: createEventDto.title,
+          description: createEventDto.description,
+          start_time: createEventDto.start_time,
+          end_time: createEventDto.end_time,
+          location: '',
+        },
+      });
+      return event;
+    } catch (error) {
+      this.logger.error('Error creating Event', error);
+      throw new HttpException(
+        {
+          status: error.status || 500,
+          error: error.message || 'Internal server error',
+        },
+        HttpStatus.BAD_REQUEST,
+        {
+          cause: error,
+          description: 'Failed to create event',
+        },
+      );
+    }
+  }
+  async findAll() {
+    try {
+      const event = await this.prismaService.events.findMany();
+      return event;
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: error.status || 500,
+          error: error.message || 'Internal Sever Error',
+        },
+        HttpStatus.BAD_REQUEST,
+        {
+          cause: error,
+          description: 'Failed to retreive events',
+        },
+      );
+    }
+  }
+  async findOne(id: string) {
+    try {
+      const event = await this.prismaService.events.findUnique({
+        where: { id },
+      });
+      if (!event) {
+        throw new NotFoundException(`Event with id ${id} not found`);
+      }
+      return event;
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: error.status || 500,
+          error: error.message || 'Internal server error',
+        },
+        HttpStatus.BAD_REQUEST,
+        {
+          cause: error,
+          description: 'Failed to retreive event',
+        },
+      );
+    }
+  }
+  async update(id: string, updateeventdto: CreateEventDto, user: JwtUser) {
+      try {
+          const event = await this.prismaService.events.findUnique({
+              where: { id },
+          });
+          if (!event) {
+              throw new NotFoundException(`Event with id ${id} not found`);
+          }
+          const updateBody = {
+              title: updateeventdto.title,
+              description: updateeventdto.description,
+              start_time: updateeventdto.start_time,
+              end_time: updateeventdto.end_time,
+          };
+          return this.prismaService.events.update({
+              where: { id },
+              data: updateBody,
+          });
+      } catch (error) {
+          throw new HttpException({
+              status: error.status || 500,
+              error: error.message || 'Internal server error',
+          },
+              HttpStatus.BAD_REQUEST,
+              {
+                  cause: error,
+                  description: 'Failed to update event',
+              },
+          );
+    }
+  }
+    async delete(id: string, user: JwtUser) {
+      try {
+          const event = await this.prismaService.events.findUnique({
+              where: { id },
+          });
+          if (!event) {
+              throw new NotFoundException('Event not found');
+          }
+          return await this.prismaService.events.delete({
+              where: { id },
+          });
+      } catch (error) {
+          throw new HttpException(
+              {
+                  status: error.status || 500,
+                  error: error.message || 'Internal server error',
+              },
+              HttpStatus.BAD_REQUEST,
+              {
+                  cause: error,
+                  description: 'Failed to delete event',
+              },
+          );
+      }
+  }
 }
